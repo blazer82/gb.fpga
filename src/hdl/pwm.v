f868056cfe39774ad3d8123c91fbc6f70976134b
Raphael Stäbler <raphael@productionbuild.de>
2021-06-21 21:24:09 +0200

    Improve audio output by inversing the pwm counter

--- src/hdl/pwm.v
+++ src/hdl/pwm.v
@@ -11,10 +11,22 @@ module pwm
     );
 
     reg[WIDTH-1:0] cnt;
+    wire[WIDTH-1:0] cnt_r;  // reversed counter, see https://zipcpu.com/dsp/2017/09/04/pwm-reinvention.html
+
+    initial begin
+        cnt <= 0;
+    end
 
     always @(posedge clk) begin
         cnt <= cnt + 1'b1;
     end
 
-    assign pwm = (digital_in > cnt);
+    genvar k;
+    generate
+        for(k=0; k<WIDTH; k=k+1) begin
+            assign cnt_r[k] = cnt[WIDTH-1-k];
+        end
+    endgenerate
+
+    assign pwm = (digital_in > cnt_r);
 endmodule
---
+++ src/hdl/pwm_tb.v
@@ -0,0 +1,30 @@
+`timescale 1ns/1ns
+
+`include "src/hdl/pwm.v"
+
+module pwm_tb;
+    reg clk;
+    reg clk2;
+    reg[7:0] cnt;
+
+    always #2 clk = !clk;
+    always #1000 clk2 = !clk2;
+
+    pwm p1 (.clk(clk), .digital_in(cnt), .pwm());
+    defparam p1.WIDTH = 8;
+
+    always @(posedge clk2) begin
+        cnt <= cnt + 1;
+    end
+
+    initial begin
+        clk <= 0;
+        clk2 <= 0;
+        cnt <= 0;
+        #1000000 $finish;
+    end
+
+    initial begin
+        $dumpvars(1, p1);
+    end
+endmodule
