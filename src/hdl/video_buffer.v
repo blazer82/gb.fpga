fe6c090d3a985059f8d1879cdb1b2db2429546d1
Raphael Stäbler <raphael@productionbuild.de>
2021-11-13 09:22:23 +0100

    Fix rgb wiring and switch to proper grayscale output

--- src/hdl/video_buffer.v
+++ src/hdl/video_buffer.v
@@ -87,6 +87,6 @@ module video_buffer
     assign addr_out = (y_in < 145) ? (144 - y_in) * 160 + (160 - x_in) : 144 * 160 + x_in;
 
     // Assign final output color based on pixel_out
-    assign color = (pixel_out == 2'b00) ? 16'hffff : ((pixel_out == 2'b01) ? 16'hcccc : ((pixel_out == 2'b10) ? 16'h6666 : 16'h0000));
+    assign color = (pixel_out == 2'b00) ? 16'hffff : ((pixel_out == 2'b01) ? 16'hce79 : ((pixel_out == 2'b10) ? 16'h632c : 16'h0000));
 
 endmodule
