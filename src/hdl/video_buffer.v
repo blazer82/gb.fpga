208170f9e592d26116099f2f3317a1599cd96dca
Raphael Stäbler <raphael@productionbuild.de>
2021-12-11 21:08:59 +0100

    Use tripple video buffer to avoid tearing

--- src/hdl/video_buffer.v
+++ src/hdl/video_buffer.v
@@ -6,7 +6,7 @@ module video_buffer
         input wire gb_de,
         input wire gb_hsync,
         input wire gb_vsync,
-        input wire[1:0] gb_pixel,
+        input wire [1:0] gb_pixel,
         input wire rst,
         input wire pclk,
         input wire de,
@@ -15,24 +15,65 @@ module video_buffer
         output wire[15:0] color
     );
 
-    reg[14:0] addr_in;
+    reg [14:0] addr_in = 0;
     reg clk_in;
-    wire[14:0] addr_out;
-    wire[1:0] pixel_out;
+    wire [14:0] addr_out;
+    
+    wire [1:0] pixel_out1;
+    wire [1:0] pixel_out2;
+    wire [1:0] pixel_out3;
+    wire [1:0] pixel_out;
 
-    reg[7:0] x_in;
-    reg[7:0] y_in;
-    reg[1:0] x_scale_cnt;
-    reg[1:0] y_scale_cnt;
+    reg [1:0] write_buffer_index = 2'b00;
+    reg [1:0] read_buffer_index = 2'b01;
 
-    blk_mem_gen_0 m1 (
+    reg [7:0] x_in;
+    reg [7:0] y_in;
+    reg [1:0] x_scale_cnt;
+    reg [1:0] y_scale_cnt;
+    
+    wire en_wr_buffer1 = write_buffer_index == 2'b00;
+    wire en_wr_buffer2 = write_buffer_index == 2'b01;
+    wire en_wr_buffer3 = write_buffer_index == 2'b10;
+    
+    wire en_rd_buffer1 = read_buffer_index == 2'b00;
+    wire en_rd_buffer2 = read_buffer_index == 2'b01;
+    wire en_rd_buffer3 = read_buffer_index == 2'b10;
+    
+    video_buffer_1 buffer1 (
         .addra(addr_in),
         .clka(clk_in),
         .dina(gb_pixel),
         .wea(1'b1),
+        .ena(en_wr_buffer1),
         .addrb(addr_out),
         .clkb(pclk),
-        .doutb(pixel_out)
+        .doutb(pixel_out1),
+        .enb(en_rd_buffer1)
+    );
+    
+    video_buffer_1 buffer2 (
+        .addra(addr_in),
+        .clka(clk_in),
+        .dina(gb_pixel),
+        .wea(1'b1),
+        .ena(en_wr_buffer2),
+        .addrb(addr_out),
+        .clkb(pclk),
+        .doutb(pixel_out2),
+        .enb(en_rd_buffer2)
+    );
+    
+    video_buffer_1 buffer3 (
+        .addra(addr_in),
+        .clka(clk_in),
+        .dina(gb_pixel),
+        .wea(1'b1),
+        .ena(en_wr_buffer3),
+        .addrb(addr_out),
+        .clkb(pclk),
+        .doutb(pixel_out3),
+        .enb(en_rd_buffer3)
     );
 
     reg [8:0] lut_addr;
@@ -52,13 +93,19 @@ module video_buffer
 
     // Count address in on each clock in, and reset on rst gb_vsync
     always @(posedge clk_in, posedge rst, posedge gb_vsync) begin
-        if (rst || gb_vsync)
-            addr_in <= 15'd0;
-        else begin
+        if (rst || gb_vsync) begin
+            if (addr_in > 11600) begin
+                if (write_buffer_index < 2'b10)
+                    write_buffer_index <= write_buffer_index + 1;
+                else
+                    write_buffer_index <= 2'b00;
+            end
+            addr_in <= 0;
+        end else begin
             if (addr_in < 23199)
-                addr_in <= addr_in + 15'd1;
+                addr_in <= addr_in + 1;
             else
-                addr_in <= 15'd0;
+                addr_in <= 0;
         end
     end
 
@@ -68,6 +115,11 @@ module video_buffer
             y_in <= 0;
             x_scale_cnt <= 0;
             y_scale_cnt <= 0;
+            case (write_buffer_index)
+                2'b00: read_buffer_index <= 2'b10;
+                2'b01: read_buffer_index <= 2'b00;
+                default: read_buffer_index <= 2'b01;
+            endcase
         end else if (!hsync) begin
             x_in <= 0;
             x_scale_cnt <= 0;
@@ -99,5 +151,7 @@ module video_buffer
     // Assign final output color based on pixel_out
     //assign color = (pixel_out == 2'b00) ? 16'hffff : ((pixel_out == 2'b01) ? 16'hce79 : ((pixel_out == 2'b10) ? 16'h632c : 16'h0000));
     assign color = (lut_color >> (pixel_out * 16)) & 16'hffff;
+    
+    assign pixel_out = en_rd_buffer1 ? pixel_out1 : (en_rd_buffer2 ? pixel_out2 : pixel_out3);
 
 endmodule
